c2c9ac9af18a348c2955679f64657cd6fac80978
mkovar <mkovar@mkovar.attlocal.net>
2019-09-11 12:12:27 -0500

    db validation to prevent submission of feedback for same order number, add new entries into db

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
 
 app = Flask(__name__)
 
@@ -29,12 +30,14 @@ class Feedback(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     customer = db.Column(db.String(200), unique=True)
+    order_number = db.Column(db.Integer, unique=True)
     employee = db.Column(db.String(200))
     rating = db.Column(db.Integer)
     comments = db.Column(db.Text())
 
-    def __init__(self, customer, employee, rating, comments):
+    def __init__(self, customer, order_number, employee, rating, comments):
         self.customer = customer
+        self.order_number = order_number
         self.employee = employee
         self.rating = rating
         self.comments = comments
@@ -50,16 +53,29 @@ def index():
 def submit():
     if request.method == 'POST':
         customer = request.form['customer']
+        order_number = request.form['orderNumber']
         employee = request.form['employee']
         rating = request.form['rating']
         comments = request.form['comments']
         # -- confirm form data
-        # print(customer, employee, rating, comments)
+        print(customer, order_number, employee, rating, comments)
 
-        if customer == '' or employee == '':
+        # -- mandatory fields cannot be empty
+        if customer == '' or employee == '' or order_number == '':
             return render_template('index.html', message='Please enter required fields')
 
-    return render_template('success.html')
+        # -- add to db if order_number does not exist, else redirect and display err msg
+        if db.session.query(Feedback).filter(Feedback.order_number == order_number).count() == 0:
+            print('SUCCESS! save new input')
+            data = Feedback(customer, order_number, employee, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            print('data should be in db')
+            return render_template('success.html')
+
+        else:
+            print('duplicate order# detected, do not save!')
+            return render_template('index.html', message="You've already submitted feedback for that order. Buy more paper, or makeup a new order number! <br/><br/> The people person's paper people's <strong>manager</strong>, <br/><em>-M. Scott</em>")
 
 
 if __name__ == '__main__':
